0f2ddcfd9f932acb4aff89081a6f82cd0ad8f332
Grace Tveit <grace.tveit@gmail.com>
2021-01-13 15:34:02 -0800

    Added Playwright::find_by_name

--- plays_orm/plays.rb
+++ plays_orm/plays.rb
@@ -92,6 +92,18 @@ class Playwright
     data.map { |datum| Playwright.new(datum) }
   end
 
+  def self.find_by_name(name)
+    data = PlayDBConnection.instance.execute(<<-SQL, name)
+      SELECT
+        *
+      FROM
+        playwrights
+      WHERE
+        name = ?
+    SQL
+    Playwright.new(data[0])
+  end
+
   def initialize(options)
     @id = options['id']
     @name = options['name']
