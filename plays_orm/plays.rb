3f3487dd142aa56c48c20114a35ab641d103d056
Grace Tveit <grace.tveit@gmail.com>
2021-01-13 15:45:54 -0800

    Added Playwright#create, Playwright#update, and Playwright#get_plays

--- plays_orm/plays.rb
+++ plays_orm/plays.rb
@@ -109,4 +109,32 @@ class Playwright
     @name = options['name']
     @birth_year = options['birth_year']
   end
+
+  def create
+    raise "#{self} already in database" if self.id
+    PlayDBConnection.instance.execute(<<-SQL, self.name, self.birth_year)
+      INSERT INTO
+        playwrights (name, birth_year)
+      VALUES
+        (?, ?)
+    SQL
+    self.id = PlayDBConnection.instance.last_insert_row_id
+  end
+
+  def update
+    raise "#{self} not in database" unless self.id
+    PlayDBConnection.instance.execute(<<-SQL, self.name, self.birth_year, self.id)
+      UPDATE
+        plays
+      SET
+        name = ?, birth_year = ?
+      WHERE
+        id = ?
+    SQL
+  end
+
+  def get_plays
+    raise "#{self} not in database" unless self.id
+    Play.find_by_playwright(self.name)
+  end
 end
