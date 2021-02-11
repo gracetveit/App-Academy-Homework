5de68a6cec73dffa5c6462ff9b9bcfd941a2d572
Grace Tveit <grace.tveit@gmail.com>
2021-02-11 11:42:03 -0800

    Completed Phases 3-5

--- rails_auth/app/models/user.rb
+++ rails_auth/app/models/user.rb
@@ -14,12 +14,46 @@ class User < ApplicationRecord
     validates :username, presence: true
     validates :password_digest, presence: true
     validates :session_token, presence: true
-    validates :password_digest, length: { minimum: 6, allow_nil: true }
+    validates :password, length: { minimum: 6, allow_nil: true }
 
-    before_validation(on: create) do
+    attr_reader :password
+
+    before_validation(on: :create) do
         self.ensure_session_token
     end
 
+    def self.find_by_credentials(username, pw)
+        errors = ActiveModel::Errors.new(User.new)
+        potential_user = User.find_by(username: username)
+        if potential_user == nil
+            raise "Could not find user with that username or password"
+        end
+        pw_object = BCrypt::Password.new(potential_user.password_digest)
+        
+        if pw_object.is_password?(pw)
+            return potential_user
+        else
+            raise "Could not find user with that username or password"
+        end
+    end
+
+    def self.generate_session_token
+        SecureRandom::urlsafe_base64
+    end
+
     def ensure_session_token
+        if self.session_token == nil
+            self.session_token = User.generate_session_token
+        end
+    end
+
+    def reset_session_token
+        self.session_token = User.generate_session_token
+        self.save
+    end
+
+    def password=(pw)
+        @password = pw
+        self.password_digest = BCrypt::Password.create(pw).to_s
     end
 end
