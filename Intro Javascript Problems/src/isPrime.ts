1d2fba5b58790e0f7a5eb6dff30b57aa33ae681a
Grace Tveit <grace.tveit@gmail.com>
2021-02-25 17:29:16 -0800

    Completed isPrime

---
+++ Intro Javascript Problems/src/isPrime.ts
@@ -0,0 +1,21 @@
+function isPrime(n:number) {
+    if (n === 1 || n === 2) {
+        return true;
+    } else if (n === 0){
+        return false;
+    }
+    
+    for (let index = 2; index < n; index++) {
+        if (n % index === 0) {
+            return false;
+        }
+    }
+    
+    return true;
+}
+
+console.log(isPrime(2));
+console.log(isPrime(0));
+console.log(isPrime(10));
+console.log(isPrime(15485863));
+console.log(isPrime(3548563));
