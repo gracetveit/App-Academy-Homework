a464a87192c1f6f48d2168f1d1adf207edaf3e23
Grace Tveit <grace.tveit@gmail.com>
2021-02-26 13:30:30 -0800

    Finished Phase V: Function Invocation

--- Intro Javascript Problems/src/Elephant.ts
+++ Intro Javascript Problems/src/Elephant.ts
@@ -25,7 +25,24 @@ class Elephant {
         console.log(`${this.name} is ${this.tricks[this.randomTrick()]}`)
     }
 
+    // paradeHelper() {
+    //     console.log(`${this.name} is trotting by!`)
+    // }
+
     private randomTrick() {
         return Math.floor(Math.random() * Math.floor(this.tricks.length));
     }
 }
+
+function paradeHelper(elephant:Elephant) {
+    console.log(`${elephant.name} is trotting by!`)
+}
+
+let ellie = new Elephant("Ellie", 185, ["giving human friends a ride", "playing hide and seek"]);
+let charlie = new Elephant("Charlie", 200, ["painting pictures", "spraying water for a slip and slide"]);
+let kate = new Elephant("Kate", 234, ["writing letters", "stealing peanuts"]);
+let micah = new Elephant("Micah", 143, ["trotting", "playing tic tac toe", "doing elephant ballet"]);
+
+let herd = [ellie, charlie, kate, micah];
+
+herd.forEach(paradeHelper);
